21a48a43878a7c8055e3374a70ceafc4827fe5c1
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2021-03-23 10:16:34 +0700

    add new project

---
+++ Portfolio3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Portfolio3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
