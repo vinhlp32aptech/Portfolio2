b2089d57e217a6271fb3d2353d5736cfff4efb55
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2021-03-23 15:23:20 +0700

    change project

---
+++ Portfolio4/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio3.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    
+    public class HomeController : Controller
+    {
+        [Route("index")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
